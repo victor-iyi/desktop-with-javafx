b02cfbf359d2c4aa3975a7c92a7fe8b88cfafd6e
victor-iyiola <javafolabi@gmail.com>
2017-08-29 12:39:25 +0100

    Initialized project

---
+++ src/com/victor/Main.java
@@ -0,0 +1,41 @@
+package com.victor;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application implements EventHandler<ActionEvent> {
+
+  private Button button;
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    primaryStage.setTitle("JavaFX GUI");
+
+    button = new Button("Click me");
+    button.setOnAction(this);
+
+    StackPane layout = new StackPane();
+    layout.getChildren().add(button);
+
+    Scene scene = new Scene(layout, 300, 250);
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+
+  @Override
+  public void handle(ActionEvent event) {
+    if (event.getSource() == button)
+      System.out.println("You clicked me!");
+  }
+
+}
