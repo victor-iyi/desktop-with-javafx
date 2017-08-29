66aaebd5d731fa2e9693b0fc207178cd8b1ab678
victor-iyiola <javafolabi@gmail.com>
2017-08-29 15:07:48 +0100

    Button anonymous inner class / lambda function

--- src/com/victor/Main.java
+++ src/com/victor/Main.java
@@ -1,16 +1,12 @@
 package com.victor;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Main extends Application implements EventHandler<ActionEvent> {
-
-  private Button button;
+public class Main extends Application {
 
   public static void main(String[] args) {
     launch(args);
@@ -21,8 +17,11 @@ public class Main extends Application implements EventHandler<ActionEvent> {
   public void start(Stage primaryStage) throws Exception {
     primaryStage.setTitle("JavaFX GUI");
 
-    button = new Button("Click me");
-    button.setOnAction(this);
+    Button button = new Button("Click me");
+    button.setOnAction(e -> {
+      System.out.println("Hello, there");
+      System.out.println("Hello, lambda smooth!");
+    });
 
     StackPane layout = new StackPane();
     layout.getChildren().add(button);
@@ -32,10 +31,5 @@ public class Main extends Application implements EventHandler<ActionEvent> {
     primaryStage.show();
   }
 
-  @Override
-  public void handle(ActionEvent event) {
-    if (event.getSource() == button)
-      System.out.println("You clicked me!");
-  }
 
 }
